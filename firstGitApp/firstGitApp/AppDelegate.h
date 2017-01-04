93e295d56e1b360aa18e1451c6b083aff569bc78
dynamic <455919447@qq.com>
2017-01-04 11:40:26 +0800

    提价第一个gitApp

---
+++ firstGitApp/firstGitApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  firstGitApp
+//
+//  Created by d y n on 2017/1/4.
+//  Copyright © 2017年 dyn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ firstGitApp/firstGitApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  firstGitApp
+//
+//  Created by d y n on 2017/1/4.
+//  Copyright © 2017年 dyn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
